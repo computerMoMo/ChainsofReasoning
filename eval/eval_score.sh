323de92dbd2f2e2630f7f3f5c3f11fdf5cfdf044
MohanZhang <mzhang13@ebay.com>
2018-07-18 20:15:26 +0800

    commit by momo

--- eval/eval_score.sh
+++ eval/eval_score.sh
@@ -3,5 +3,5 @@ alpha_array=("0.0" "0.2" "0.4" "0.6" "0.8" "1.0")
 for alpha in ${alpha_array[@]}
 do
   echo "evaluate on "alpha
-  python eval_score.py $alpha $1 $2 $3
+  python eval_score.py $alpha $1 $2
 done
