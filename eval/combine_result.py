9564a4ee70b51ae85dabaa11b7c7f28aefefba0b
MohanZhang <mzhang13@ebay.com>
2018-07-18 20:14:22 +0800

    commit by momo

--- eval/combine_result.py
+++ eval/combine_result.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     entity_reader = codecs.open(os.path.join(entity_dir, sys.argv[1]), mode="r", encoding="utf-8")
     # head_line = entity_reader.readline()
     result_reader = codecs.open(sys.argv[2], mode="r", encoding="utf-8")
-    pos_combine_writer = codecs.open(sys.argv[3]+"_pos.txt", mode="w", encoding="utf-8")
-    neg_combine_writer = codecs.open(sys.argv[3] + "_neg.txt", mode="w", encoding="utf-8")
+    combine_writer = codecs.open(sys.argv[3]+"_combine.txt", mode="w", encoding="utf-8")
 
     count_num = 0
     entity_line = entity_reader.readline()
@@ -19,11 +18,7 @@ if __name__ == "__main__":
     while entity_line and result_line:
         entity_list = entity_line.strip().split("\t")
         result_list = result_line.strip().split("\t")
-        if result_list[-1] == "0":
-            neg_combine_writer.write(entity_list[1]+"\t"+entity_list[2]+"\t"+result_list[-1]+"\t"+result_list[-2]+"\n")
-        else:
-            pos_combine_writer.write(entity_list[1]+"\t"+entity_list[2]+"\t"+result_list[-1]+"\t"+result_list[-2]+"\n")
-
+        combine_writer.write(entity_list[1]+"\t"+entity_list[2]+"\t"+result_list[-1]+"\t"+result_list[-2]+"\n")
         count_num += 1
         if count_num % 10000 == 0:
             print(count_num)
@@ -32,5 +27,4 @@ if __name__ == "__main__":
         result_line = result_reader.readline()
     entity_reader.close()
     result_reader.close()
-    pos_combine_writer.close()
-    neg_combine_writer.close()
+    combine_writer.close()
--- eval/concat.sh
+++ eval/concat.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-echo "cat positive file"
-pos_file_list_str="test_pos.txt "
+echo "cat file"
+pos_file_list_str="test_combine.txt "
 for i in {1..13}
 do
-    pos_file_list_str=$pos_file_list_str" test_"$i"_pos.txt"
+    pos_file_list_str=$pos_file_list_str" test_"$i"_combine.txt"
 done
-cat $pos_file_list_str > "total_pos.txt"
+cat $pos_file_list_str > "total_combine.txt"
 
-echo "cat negative file"
-neg_file_list_str="test_neg.txt "
-for i in {1..13}
-do
-    neg_file_list_str=$neg_file_list_str" test_"$i"_neg.txt"
-done
-cat $neg_file_list_str > "total_neg.txt"
+#echo "cat negative file"
+#neg_file_list_str="test_neg.txt "
+#for i in {1..13}
+#do
+#    neg_file_list_str=$neg_file_list_str" test_"$i"_neg.txt"
+#done
+#cat $neg_file_list_str > "total_neg.txt"
