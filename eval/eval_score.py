3ea0fadbc3152cebf0320dfde2b81ce3e2b7b373
MohanZhang <mzhang13@ebay.com>
2018-07-18 20:02:54 +0800

    commit by momo

--- eval/eval_score.py
+++ eval/eval_score.py
@@ -104,7 +104,6 @@ if __name__ == "__main__":
                 ground_truth_labels.append(0.0)
                 predict_labels.append(float(_scores[1]))
 
-            _hit, _ndcg = eval_one_rating(i_gnd=ground_truth_labels, i_pre=predict_labels, K=15)
             temp_hit = []
             temp_ndcg = []
             for k in range(1, 16):
