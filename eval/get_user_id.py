f202e03e583bd11444b3cf821898e6964e11f177
MohanZhang <mzhang13@ebay.com>
2018-07-19 16:29:47 +0800

    commit by momo

--- eval/get_user_id.py
+++ eval/get_user_id.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 from __future__ import print_function
 import codecs
+import sys
 
 if __name__ == "__main__":
     pos_user_id_list = []
-    pos_reader = codecs.open("total_pos.txt", mode="r", encoding="utf-8")
+    pos_reader = codecs.open(sys.argv[1], mode="r", encoding="utf-8")
     line = pos_reader.readline()
     while line:
         line_list = line.strip().split("\t")
@@ -16,7 +17,7 @@ if __name__ == "__main__":
     print(len(pos_user_id_list))
 
     pos_user_id_list = sorted(list(pos_user_id_list))
-    user_id_writer = codecs.open("user_id.txt", mode="w", encoding="utf-8")
+    user_id_writer = codecs.open(sys.argv[2], mode="w", encoding="utf-8")
     for user_id in pos_user_id_list:
         user_id_writer.write(str(user_id)+"\n")
     user_id_writer.close()
