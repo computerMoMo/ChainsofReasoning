b46ace2d10504884f142859874c4e2c6fc82ce81
MohanZhang <mzhang13@ebay.com>
2018-07-20 17:10:08 +0800

    commit by momo

--- eval/eval.sh
+++ eval/eval.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 mode_path=$1
 result_path=$2
+echo"model testing..."
 bash model_test.sh $mode_path "0" $result_path
 bash combine_result.sh $result_path
 bash concat.sh $result_path
 python resort.py $result_path"/total_combine.txt" $result_path"/total_combine_sorted.txt"
 bash eval_socre.sh $result_path"/total_combine_sorted.txt" $result_path
+
